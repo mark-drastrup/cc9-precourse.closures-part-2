a672cf8b376b9e46ee2262858ebf023f896ceffb
MarkDrastrup <mark.drastrup@gmail.com>
2019-06-01 22:20:46 +0200

    Refactoring 'gameGenerator' tests to instantiate game in beforeEach

--- specs/tests.js
+++ specs/tests.js
@@ -1,11 +1,15 @@
 describe("gameGenerator", () => {
+  let game;
+
+  beforeEach(function() {
+    game = gameGenerator(5);
+  })
   it("should be there", () => {
     expect(gameGenerator).toBeDefined();
     expect(typeof gameGenerator).toBe("function");
   });
 
   it("should generate some games!", () => {
-    const game = gameGenerator(4);
     expect(typeof game).toBe("object");
   });
 
@@ -22,30 +26,25 @@ describe("gameGenerator", () => {
   });
 
   it("should have a reset method", () => {
-    const game = gameGenerator(3);
     expect(game.reset).toBeDefined();
     expect(typeof(game.reset)).toBe('function');
   });
 
   it("should have a giveUp method", () => {
-    const game = gameGenerator(3);
     expect(game.giveUp).toBeDefined();
     expect(typeof(game.giveUp)).toBe('function');
   });
 
   it("The giveUp function should set a new winning number", () => {
-    const game = gameGenerator(5);
     expect(game.giveUp()).not.toEqual(answer)
   })
 
   it("should have a numberGuesses method", () => {
-    const game = gameGenerator(5);
     expect(game.numberGuesses).toBeDefined();
     expect(typeof(game.numberGuesses)).toBe('function');
   });
 
   it("should return the number of guesses, when the numberGuesses function is invoked", () => {
-    const game = gameGenerator(5);
     let guesses = [];
     for(let i = 0; i < 5; i++) {
       game.guess(i);
